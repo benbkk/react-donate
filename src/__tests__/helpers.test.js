e2a28d294484b0b9197a5fda63c8b618e59a128c
Beta Anggoro <ben@benworkandplay.com>
2018-01-10 05:30:49 +0700

    Fixed test

--- src/__tests__/helpers.test.js
+++ src/__tests__/helpers.test.js
@@ -1,4 +1,4 @@
-import { summaryDonations } from '../helpers';
+import { summaryDonations } from 'utils';
 
 describe('helpers', function() {
   test('`summaryDonations` should calculate donations correctly', function() {
